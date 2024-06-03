36da8f6b0f220b0e326726f7970cd2887fa187bc
iacta <thiagobahiavitor@gmail.com>
2024-06-03 17:57:41 -0300

    new

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,6 @@
 import { Inter } from "next/font/google";
+import { openGraphImage } from '../../public/bgl.png'
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -6,11 +8,15 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Brasil Gamer Life",
   description: "Brasil Gamer Life RPG - Servidor SAMP",
+  openGraph: {
+    ...openGraphImage
+  }
+
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={`${inter.className} bg-black text-white`}>{children}</body>
     </html>
   );
